c931e7eabb11b7d276a7430f7f0e0a16ac53b4af
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2022-10-30 12:55:36 +0530

    Implementing HomeUI and LoginUI

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 // In App.js in a new project
 
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./components/home/Home";
 import Login from "./components/login/Login";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-	return (
-		<NavigationContainer>
-			<Stack.Navigator screenOptions = {{headerShown : false}}>
-				<Stack.Screen name="Login" component={Login} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
