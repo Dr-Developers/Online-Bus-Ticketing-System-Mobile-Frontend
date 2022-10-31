2b0431ee13178c49e86f23f0b7df141e435f89c2
Heshani Sandanika <74381597+heshani9918@users.noreply.github.com>
2022-11-01 01:09:29 +0530

    Implementing inspection style

---
+++ components/inspection/style.js
@@ -0,0 +1,73 @@
+import { StyleSheet } from "react-native";
+import { darkGrey, darkRed } from "../../colors/Colors";
+
+const styles = StyleSheet.create({
+  login: {
+    color: "white",
+    fontSize: 64,
+    fontWeight: "bold",
+    marginVertical: 50,
+  },
+  regist: {
+    color: "white",
+    fontSize: 56,
+    fontWeight: "bold",
+    marginTop: 33,
+  },
+  container: {
+    alignItems: "center",
+    width: 400,
+    marginTop: 50,
+  },
+  registerContainer: {
+    backgroundColor: "white",
+    height: 500,
+    width: 380,
+    borderTopLeftRadius: 70,
+    borderTopRightRadius: 70,
+    borderBottomLeftRadius: 70,
+    borderBottomRightRadius: 70,
+    paddingTop: 50,
+    alignItems: "center",
+  },
+  heading: {
+    fontSize: 16,
+    color: "white",
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  subHeading: {
+    color: "gray",
+    fontSize: 13,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  forgetHolder: {
+    alignItems: "flex-end",
+    width: "78%",
+    paddingRight: 16,
+    marginBottom: 30,
+  },
+  forget: {
+    color: darkRed,
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  footerHolder: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  account: {
+    color: darkGrey,
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  register: {
+    color: darkRed,
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
+
+export default styles;
