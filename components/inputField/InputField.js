8ebc849e3c1cbdac56dc58d81320d4401aa07515
Poorna Sankalana <83366788+PoornaSankalana@users.noreply.github.com>
2022-11-01 15:23:49 +0530

    Text input field component implementation

--- components/inputField/InputField.js
+++ components/inputField/InputField.js
@@ -3,21 +3,20 @@ import { TextInput } from "react-native";
 import { darkGrey } from "../../colors/Colors";
 
 const InputField = (props) => {
-  return (
-    <TextInput
-      {...props}
-      style={{
-        borderRadius: 100,
-        color: { darkGrey },
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        width: "80%",
-        backgroundColor: "rgb(220,220,220)",
-        marginVertical: 10,
-      }}
-      placeholderTextColor={darkGrey}
-    ></TextInput>
-  );
+	return (
+		<TextInput
+			{...props}
+			style={{
+				borderRadius: 100,
+				color: { darkGrey },
+				paddingHorizontal: 20,
+				paddingVertical: 10,
+				width: "80%",
+				backgroundColor: "rgb(220,220,220)",
+				marginVertical: 9,
+			}}
+			placeholderTextColor={darkGrey}></TextInput>
+	);
 };
 
 export default InputField;
